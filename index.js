fe8aa3f7d5a27f0133bc03e6f726847ab35499ba
Aston Jones <astonbradleyjones@gmail.com>
2022-01-10 21:16:40 -0600

    server runs correctly

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const axios = require('axios');
+const http = require('http');
 
 const app = express();
 const PORT = process.env.PORT || 5000
 const server = http.createServer(app).listen(PORT, () => console.log
-(`Listening on ${port}`))
+(`Listening on ${PORT}`))
