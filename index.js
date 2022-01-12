cbb19383853f3f1c4946aec8f1f4b8852979d9cd
Aston Jones <astonbradleyjones@gmail.com>
2022-01-12 17:11:51 -0600

    Made server tick every 3 seconds.

--- index.js
+++ index.js
@@ -9,3 +9,7 @@ const app = express();
 const PORT = process.env.PORT || 5000
 const server = http.createServer(app).listen(PORT, () => console.log
 (`Listening on ${PORT}`))
+
+// Check markets every n seconds
+const POLLING_INTERVAL = process.env.POLLING_INTERVAL || 3000 // 3 Seconds
+priceMonitor = setInterval(async () => { console.log("Loading") }, POLLING_INTERVAL)
